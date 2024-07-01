5eea3ebbac86370be624fbcf008527386d7b8296
Himanshu X Ops <himanshuxops55@gmail.com>
2024-07-01 12:45:26 +0530

    http module

---
+++ httpmodule.js
@@ -0,0 +1,54 @@
+// const http = require('http')
+// const port = 8080;
+// const server = http.createServer((req,res)=>{
+//     res.write('the form submit')
+//     res.end()
+
+// })
+
+// server.listen(port,()=>{
+//     console.log("run this server on port 8080")
+// })
+
+
+
+// // import the http module :-
+
+// const http = require('http')
+
+// // define the port :-
+// const port = 8080;
+
+// // create a http server:-
+
+// const server = http.createServer((req,res)=>{
+//     res.write ('mission accomplish')
+//     res.end()
+
+// })
+
+// // start the Server
+
+// server.listen (port,()=>{
+//     console.log("run code 8080");
+// });
+const http = require('http')
+const port = 8080;
+const server = http.createServer(()=>{
+    "work done"
+
+})
+
+server.listen(port,()=>{
+    console.log("run this port 8080");
+})
+
+const http =  require('http')
+const port = 8080;
+const server = http.createServer(()=>{
+    "this work done "
+})
+
+server.listen(port,()=>{
+    console.log("run this port 8080");
+})
